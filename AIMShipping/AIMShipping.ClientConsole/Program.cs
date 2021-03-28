9143410ffa5f8622fb8003e388b165f83c9078f1
Ahmed Ilyas <v-ahily@microsoft.com>
2021-03-28 14:54:02 -0700

    Commit

---
+++ AIMShipping/AIMShipping.ClientConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AIMShipping.ClientConsole
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
